10d7aeede19663af530b5d9518e3ff278786a814
Hithun Harindran <hithunharindran@gmail.com>
2019-11-07 07:44:00 +0000

    assignment
    Change-Id: Ibd8614480e42a9cdc294ef95ed0623cb72390a92

---
+++ webapp/Formatters/reusable.js
@@ -0,0 +1,28 @@
+sap.ui.define([], function () {
+return {
+	fnFamilyStatus :function(family){
+		if(family ==="Y"){
+			return "Yes";
+		}
+		else{
+			return "No";
+		}
+	},
+	
+	fnDOB :function(dob){
+	if (dob === null){
+		return dob;
+			
+	
+	}
+	else{
+			return dob.substring(0,2) + "." + dob.substring(2,4) + "." + dob.substring(4);
+	}
+	
+
+}
+
+
+};
+	
+});
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,20 +1,19 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"webdesign/Web_design/Formatters/reusable"
+], function (Controller,reuse) {
 	"use strict";
 
 	return Controller.extend("webdesign.Web_design.controller.View1", {
 		onInit: function () {
-
-// save
-//2 nd
-//3 
-//4
-
-			
-			// save by swap13nil
-
-		}
+        var oTable = this.getView().byId("table1");
+        var oModel = new sap.ui.model.json.JSONModel();
+        oModel.loadData("ContactData/CData.json");
+        oTable.setModel(oModel);
+		},
+		
+		test : reuse
+		
 	});
 	
 	
